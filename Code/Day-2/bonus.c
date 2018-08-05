683c56a71a25d58636ebc9d4d37961c8a2594fef
nusrat0927 <40735231+nusrat0927@users.noreply.github.com>
2018-08-05 16:47:57 +0530

    Day-2

---
+++ Code/Day-2/bonus.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int bonus,yos,cy,yoj;
+ printf("enter the yoj and cy");
+ scanf("%d%d",&yoj,&cy);
+ yos=cy-yoj;
+  if(yos>3)
+  {
+   bonus=2500
+   printf("bonus=%d\n",bonus);
+  }
+  return 0;
+  }
