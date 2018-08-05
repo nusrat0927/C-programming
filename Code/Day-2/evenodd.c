87fbc706fa3156bb3a524338b4ee7d538738ab25
nusrat0927 <40735231+nusrat0927@users.noreply.github.com>
2018-08-05 16:35:51 +0530

    Day-2

---
+++ Code/Day-2/evenodd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main
+{
+ int a;
+ printf("Enter a number");
+ scanf("%d",&a);
+  if(a%2==0)
+  { 
+   printf("number is even");
+  }
+  else
+   printf("number is odd");
+  getch();
+} 
