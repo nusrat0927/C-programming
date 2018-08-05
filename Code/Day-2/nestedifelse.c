fd0c62c9fe8171422af91b708d5d0c2e916a919e
nusrat0927 <40735231+nusrat0927@users.noreply.github.com>
2018-08-05 16:54:12 +0530

    Day-2

---
+++ Code/Day-2/nestedifelse.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int i;
+printf("enter either 1 or 2");
+scanf("%d",&i);
+if(i==1)
+{
+printf("you will top");
+}
+else
+{
+if(i==2)
+{
+printf("you will simply pass");
+else
+{
+printf("you will fail");
+}
+return 0;
+}
